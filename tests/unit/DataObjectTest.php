480e10030853ee8e321c52d0e589ccb126a246d8
Michael Riedmann <reddragon010@gmx.net>
2012-10-25 19:38:25 +0200

    renaming / added empty test

---
+++ tests/unit/DataObjectTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mriedmann
+ * Date: 25.10.12
+ * Time: 19:35
+ * To change this template use File | Settings | File Templates.
+ */
+class DataObjectTest
+{
+
+}
